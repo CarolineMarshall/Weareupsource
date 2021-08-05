8f296b55ad857846a7e13f68e62f153d0c98dc9d
Mariareynolds <mfrmostyn@hotmail.co.uk>
2021-08-05 01:00:10 +0100

    Added team page content and styling

--- _site/javascript/readmore.js
+++ _site/javascript/readmore.js
@@ -1,10 +1,9 @@
 
 
-
+// alert(target)
 
 $(".team-member").click(function(){
      var target = $(this).attr("data-target");
-    alert(target)
     $(".list").each(function(){
       if($(this).attr("data-list") == target){
         $(this).slideToggle();
